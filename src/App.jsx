545a21e41c716d0398dee6d46cbc5267009d8928
rick <philipesferreiraa@gmail.com>
2022-12-27 01:42:40 -0300

    renderizando os pontos=dot na tela

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([]);
+
+  const handleClick = (event) => {
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY
+    }
+    setList((prev) => [...prev, newDot]);
+  }
 
   return (
-    <div id='page'> Testando </div>
+    <div id='page' onClick={handleClick}>
+      {list.map((item) => <span className='dot'
+        style={{ left: item.clientX, top: item.clientY }} />)}
+    </div>
   )
 }
 
